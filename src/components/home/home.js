c9a20cf83772c3ee352a35f6e18f367bf68576eb
mattenbar <matthw.enbar@gmail.com>
2021-03-09 08:59:47 -0500

    links bar creats no CSS to it yet

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,10 +2,15 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 // import {Link} from "react-router-dom"
 
+//importing components for home
+import LinksBar from './linksBar'
+
 class Home extends Component {
   render(){
     return(
+      
       <div>
+        <LinksBar />
         We are in Home component
       </div>
     )
