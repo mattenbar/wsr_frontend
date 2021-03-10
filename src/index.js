6ffffb1d7904847cc7787da53aa1da62ac017366
osha7 <ogroetz@gmail.com>
2021-03-09 19:16:48 -0700

    tab icon changed to WSR logo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 
 import './css/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import thunk from 'redux-thunk'
 import {BrowserRouter as Router} from 'react-router-dom'
@@ -26,7 +25,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
