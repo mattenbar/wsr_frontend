4ad45ecd8ae02f28a728c05c79f50fd25cca0bb7
osha7 <ogroetz@gmail.com>
2021-03-11 13:32:20 -0700

    carousel working

--- src/components/home/carousel.js
+++ src/components/home/carousel.js
@@ -1,10 +1,88 @@
 import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function carousel(props) {
+    let settings = {
+        arrows: true,
+        // prevArrow:"<img class='a-left control-c prev slick-prev' src='carouselImages/arrow-left.png'>",
+        // nextArrow:"<img class='a-right control-c next slick-next' src='carouselImages/arrow-right.png'>",
+        dots: false,
+        infinite: true, 
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        cssEase: "linear"
+    }
     return (
-        <div>
-            
-        </div>
+        <Slider {...settings} >
+            <div className="carousel-wrapper" >
+                <div className="box">
+                    {/* model */}
+                    <a href="#" ><img src="carouselImages/pointCounterpoint.png" class="model" /></a>
+                    {/* header */}
+                    <h2>CAPITAL CONNECTIONS</h2>
+                </div>
+            </div>
+            <div className="carousel-wrapper" >
+                <div className="box">
+                    {/* model */}
+                    <a href="#" ><img src="carouselImages/capitalConnections.png" class="model" /></a>
+                    {/* header */}
+                    <h2>CAPITAL CONNECTIONS</h2>
+                </div>
+            </div>
+            <div className="carousel-wrapper" >
+                <div className="box">
+                    {/* model */}
+                    <a href="#" ><img src="carouselImages/digitalDomain.png" class="model" /></a>
+                    {/* header */}
+                    <h2>CAPITAL CONNECTIONS</h2>
+                </div>
+            </div>
+            <div className="carousel-wrapper" >
+                <div className="box">
+                    {/* model */}
+                    <a href="#" ><img src="carouselImages/shoutOuts.png" class="model" /></a>
+                    {/* header */}
+                    <h2>CAPITAL CONNECTIONS</h2>
+                </div>
+            </div>
+            <div className="carousel-wrapper" >
+                <div className="box">
+                    {/* model */}
+                    <a href="#" ><img src="carouselImages/pointCounterpoint.png" class="model" /></a>
+                    {/* header */}
+                    <h2>CAPITAL CONNECTIONS</h2>
+                </div>
+            </div>
+            <div className="carousel-wrapper" >
+                <div className="box">
+                    {/* model */}
+                    <a href="#" ><img src="carouselImages/capitalConnections.png" class="model" /></a>
+                    {/* header */}
+                    <h2>CAPITAL CONNECTIONS</h2>
+                </div>
+            </div>
+            <div className="carousel-wrapper" >
+                <div className="box">
+                    {/* model */}
+                    <a href="#" ><img src="carouselImages/digitalDomain.png" class="model" /></a>
+                    {/* header */}
+                    <h2>CAPITAL CONNECTIONS</h2>
+                </div>
+            </div>
+            <div className="carousel-wrapper" >
+                <div className="box">
+                    {/* model */}
+                    <a href="#" ><img src="carouselImages/shoutOuts.png" class="model" /></a>
+                    {/* header */}
+                    <h2>CAPITAL CONNECTIONS</h2>
+                </div>
+            </div>
+
+        </Slider>
     );
 }
 
