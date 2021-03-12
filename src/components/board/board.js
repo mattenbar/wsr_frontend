ca4d66a1cebc016324c4e4a6a53d612433b54b58
mattenbar <matthw.enbar@gmail.com>
2021-03-12 12:15:48 -0500

    more board members created

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -3,13 +3,28 @@ import React, {Component} from 'react';
 //import board members
 import MattBrown from './mattBrown'
 import BrendanClark from './brendanClark'
+import LisaGok from './lisaGok'
+import HarryGrand from './harryGrand'
+import JeffNash from './jeffNash'
+import MarryOsaka from './marryOsaka'
+import ChrisPaulitz from './chrisPaulitz'
+import JimRoth from './jimRoth'
+import SusanThedar from './susanThedar'
 
 class Board extends Component{
   render(){
     return(
       <div className="board-container">
+        <h1>Our Board</h1>
         <MattBrown />
         <BrendanClark />
+        <LisaGok />
+        <HarryGrand />
+        <JeffNash />
+        <MarryOsaka />
+        <ChrisPaulitz />
+        <JimRoth />
+        <SusanThedar />
       </div>
     )
   }
---
+++ src/components/board/marryOsaka.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import marryOsakaImage from './images/marryOsaka.jpg'
+
+function marryOsaka(props) {
+  return (
+      <div>
+        <p>
+        <img src={marryOsakaImage}/>
+          <br/>
+          Mary Osako is UCLA’s inaugural vice chancellor for strategic communications. A Los Angeles native and UCLA alumna, Mary has more than 20 years of experience leading complex communications efforts at some of the most innovative Fortune 500 companies in the world. She is well-versed in all facets of communications, reputation management and brand marketing, encompassing media relations, crisis communications, public affairs, executive communications, events and social and digital marketing.
+          Previously, Mary served as chief communications officer of Activision Blizzard; head of global corporate communications at Amazon; and vice president of corporate, international and public policy communications at Yahoo!. Immediately prior to joining UCLA, she was a partner and chief operating officer at Haven Tower Group, a leading strategic communications and marketing firm based in Santa Monica that serves corporate clients across the country, where she currently serves as vice chair of the firm’s board of advisors.
+          Mary serves on the board of directors of the Hammer Museum in Los Angeles. She earned her bachelor’s degree in psychology and a specialization in Asian American studies from UCLA in 1996.
+
+        </p>
+      </div>
+  );
+}
+
+export default marryOsaka;
