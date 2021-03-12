0c6f836312ef6fafcd204f27b9d1cb565771fa01
osha7 <ogroetz@gmail.com>
2021-03-12 12:48:32 -0700

    spacing in links home page top

--- src/components/home/linksBar.js
+++ src/components/home/linksBar.js
@@ -3,25 +3,28 @@ import React, {Component} from 'react'
 class LinksBar extends Component {
   render(){
     return(
-      <div className="linksBar">
-        <a href="">POINT-COUNTERPOINT</a> &nbsp;
-        <a href="">CAPITAL CONNECTIONS</a>&nbsp;
-        <a href="">DIGITAL DOMAIN</a>&nbsp;
-        <a href="">SHOUT OUTS & PUT DOWNS</a>&nbsp;
-        <a href="">BELTWAY AND BEYOND</a>&nbsp;
-        <a href="">LAWYER LAND</a>&nbsp;
-        <a href="">NIGHTMARE ON COMPLIANCE ST</a>&nbsp;
-        <br />
-        <a href="">BUY IT OR BUILD IT?</a>&nbsp;
-        <a href="">WALLET SHARE</a>&nbsp;
-        <a href="">FAMILY FEUDS</a>&nbsp;
-        <a href="">PRODUCT STRATEGIES</a>&nbsp;
-        <a href="">LETTERS ROM LARRY</a>&nbsp;
-        <a href="">NOTEWORTHY NOTES</a>&nbsp;
-        <a href="">NEWSMAKERS ROUNDUP</a>&nbsp;
-        <a href="">TRANSACTIONS AND TRANSITIONS</a>&nbsp;
-        <a href="">FROM OUR PARTNERS</a>
+      <>
+      <div className="linksBar-top">
+          <a href="">POINT-COUNTERPOINT</a> &nbsp;
+          <a href="">CAPITAL CONNECTIONS</a>&nbsp;
+          <a href="">DIGITAL DOMAIN</a>&nbsp;
+          <a href="">SHOUT OUTS & PUT DOWNS</a>&nbsp;
+          <a href="">BELTWAY AND BEYOND</a>&nbsp;
+          <a href="">LAWYER LAND</a>&nbsp;
+          <a href="">NIGHTMARE ON COMPLIANCE ST</a>&nbsp;
+        </div>
+        <div className="linksBar-bottom">
+          <a href="">BUY IT OR BUILD IT?</a>&nbsp;
+          <a href="">WALLET SHARE</a>&nbsp;
+          <a href="">FAMILY FEUDS</a>&nbsp;
+          <a href="">PRODUCT STRATEGIES</a>&nbsp;
+          <a href="">LETTERS ROM LARRY</a>&nbsp;
+          <a href="">NOTEWORTHY NOTES</a>&nbsp;
+          <a href="">NEWSMAKERS ROUNDUP</a>&nbsp;
+          <a href="">TRANSACTIONS AND TRANSITIONS</a>&nbsp;
+          <a href="">FROM OUR PARTNERS</a>
       </div>
+      </>
     )
   }
 }
