9a03af75d731c204fc3972f269bd71d9998d390a
osha7 <ogroetz@gmail.com>
2021-03-18 22:34:05 -0600

    signup/in pop ups

--- src/components/navbar/popUpSignIn.js
+++ src/components/navbar/popUpSignIn.js
@@ -5,8 +5,11 @@ function popUpSignIn(props) {
     return (
         <div className="popup" id="popup2"> 
             <div className="popup__content">
+                <div className="leftpopupleft" ></div>
                 <div className="popup__left">
-
+                        <h1>Wealth</h1>
+                        <h1>Solutions</h1>
+                        <h1>Report</h1>
                 </div>
                 <div className="popup__right" >
                     <a href="#" className="popup__close">&times;</a>
