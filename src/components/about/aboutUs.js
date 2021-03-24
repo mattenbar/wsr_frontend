76abe4212360ea01dd4a16e302c3bb097060c592
mattenbar <matthw.enbar@gmail.com>
2021-03-24 10:02:30 -0400

    about us section complete

--- src/components/about/aboutUs.js
+++ src/components/about/aboutUs.js
@@ -6,6 +6,13 @@ function aboutUs(props) {
         <div className="board-header">
           <h1>ABOUT US</h1>
         </div>
+        <div className="about-us">
+          Wealth Solutions Report is a digital media company founded by Larry Roth, former CEO of AIG Advisor Group, CEO of Cetera Financial Group, Board Director at Oppenheimer & Company, and Investment Banker Affiliated with Berkshire Global Advisors. 
+          <br/><br/>
+          We provide cutting-edge insights, actionable analysis, and expert commentary on the most relevant and timely issues that are shaping the future of wealth management. We engage, inspire, and connect with firms and individuals across the wealth management space, while serving as a bridge between sources of private growth capital and wealth management businesses of all sizes and models. 
+          <br/><br/>
+          Leveraging robust interactive digital engagement and communications tools, we deliver original and exclusive content directly from the most reputable thought leaders associated with the wealth management space. All content emphasizes actionable ideas and strategies to drive business growth with a bias for seeing growth opportunities and a savvy, can-do mindset.
+        </div>
         
       </div>
     );
