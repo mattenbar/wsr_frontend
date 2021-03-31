495bf54b2a69d7a46d1357ffcd4d189b1754694a
mattenbar <matthw.enbar@gmail.com>
2021-03-31 17:57:40 -0400

    created linksbar for each week

---
+++ src/components/home/linksbars/linksBarWeekOne.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+
+class linksBarWeekOne extends Component {
+  render(){
+    return(
+      <div className="linksBarWeekOne-container">
+
+
+          <a href="/Letters-From-Larry">LETTERS FROM LARRY</a>&nbsp;
+          <a href="/Capital-Connections">CAPITAL CONNECTIONS</a>&nbsp;
+          <a href="/Point-Counterpoint">POINT-COUNTERPOINT</a> &nbsp;
+          <a href="/The-Digital-Domain">THE DIGITAL DOMAIN</a>&nbsp;
+          <a href="/Words-In-Edgewise">WORDS IN EDGEWISE</a>&nbsp;
+          <a href="/Wallet-Share">WALLET SHARE</a>&nbsp;
+          {/* statshots will go here */}
+
+         
+      </div>
+    )
+  }
+}
+
+export default linksBarWeekOne
---
+++ src/components/home/linksbars/linksBarWeekTwo.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+
+class LinksBarWeekTwo extends Component {
+  render(){
+    return(
+      <div className="linksBarWeekTwo-container">
+
+          <a href="/Letters-From-Larry">LETTERS FROM LARRY</a>&nbsp;
+          <a href="/Capital-Connections">CAPITAL CONNECTIONS</a>&nbsp;
+          <a href="/Point-Counterpoint">POINT-COUNTERPOINT</a> &nbsp;
+          <a href="/The-Digital-Domain">THE DIGITAL DOMAIN</a>&nbsp;
+          <a href="/Words-In-Edgewise">WORDS IN EDGEWISE</a>&nbsp;
+          <a href="/Wallet-Share">WALLET SHARE</a>&nbsp;
+          {/* statshots will go here */}
+
+          {/* week two */}
+
+          <a href="/NewsMakers-Roundup">NEWSMAKERS ROUNDUP</a>&nbsp;
+          <a href="/Bizarre-Industry-Bazaar">BIZARRE INDUSTRY BAZAAR</a>&nbsp;
+          <a href="/Investment-Solutions-and-Gatekeepers">INVESTMENT SOLUTIONS & GATEKEEPERS</a>&nbsp;
+          <a href="/On-The-Rise">ON THE RISE</a>&nbsp;
+      </div>
+    )
+  }
+}
+
+export default LinksBarWeekTwo
