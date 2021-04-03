d4f517a0afa1bc1ddd6af87a0276372634fad9e5
mattenbar <matthw.enbar@gmail.com>
2021-04-03 15:44:33 -0400

    single article pcp css done

--- src/components/pointcp/pointCounterPointArticle.js
+++ src/components/pointcp/pointCounterPointArticle.js
@@ -1,151 +1,163 @@
 import React, { useState, useEffect } from "react";
 import { API_URL } from "../../apiConstants";
-import { useSelector, useDispatch } from 'react-redux';
-import PointcpArticleBody from './pointcpArticleBody';
-import ArticleHeaders from '../articles/articleHeaders';
-import PointcpEditForm from './pointcpEditForm';
-import { deletePointcp } from '../../actions/pointcp/deletePointcp';
+import { useSelector, useDispatch } from "react-redux";
+import PointcpArticleBody from "./pointcpArticleBody";
+import ArticleHeaders from "../articles/articleHeaders";
+import PointcpEditForm from "./pointcpEditForm";
+import { deletePointcp } from "../../actions/pointcp/deletePointcp";
 import { withRouter } from "react-router";
 import { useLocation } from "react-router-dom";
-import history from '../../history';
+import history from "../../history";
+import { Link } from "react-router-dom";
 import { voteArticleOne, voteArticleTwo } from "../../actions/pointcp/addVotes";
 
 function PointCounterPointArticle(props) {
+  const user = useSelector((state) => {
+    return state.user.user;
+  });
 
-    const user = useSelector(state => {
-        return (state.user.user)
-    })
+  const user_id = user.id;
 
-    const user_id = user.id
+  let admin;
 
-    let admin
+  if (user !== undefined) {
+    admin = user.admin;
+  }
 
-    if (user !== undefined) {
-        admin = user.admin
+  let location = useLocation();
+
+  let pointcp_id;
+  if (props.location.state) {
+    pointcp_id = props.location.state.pointcp_id;
+  }
+
+  const [pointcpArticle, setPointcpArticle] = useState([]);
+  const [winner, setWinner] = useState("");
+
+  const [inEditMode, setInEditMode] = useState(false);
+
+  useEffect(() => {
+    if (pointcp_id) {
+      fetch(API_URL + `/pointcps/${pointcp_id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setPointcpArticle(data.pointcp.data);
+          setWinner(data.winner);
+        });
+    }
+  }, []);
+
+  const handleVotingClickButtonOne = (e) => {
+    e.preventDefault();
+
+    if (!localStorage.token) {
+      alert("Please sign in to vote");
     }
 
-    let location = useLocation();
+    if (localStorage.token !== undefined) {
+      let pointcpData = {
+        pointcp_id: pointcp_id,
+        user_id: user_id,
+        articleOneVote: 1,
+        articleTwoVote: 0,
+      };
 
-    let pointcp_id
-    if (props.location.state) {
-        pointcp_id = props.location.state.pointcp_id
+      dispatch(voteArticleOne(pointcpData));
+    }
+  };
+
+  const handleVotingClickButtonTwo = (e) => {
+    e.preventDefault();
+
+    if (!localStorage.token) {
+      alert("Please sign in to vote");
     }
 
+    if (localStorage.token !== undefined) {
+      let pointcpData2 = {
+        pointcp_id: pointcp_id,
+        user_id: user_id,
+        articleOneVote: 0,
+        articleTwoVote: 1,
+      };
 
-    const [pointcpArticle, setPointcpArticle] = useState([]);
-    const [winner, setWinner] = useState('')
-
-    const [inEditMode, setInEditMode] = useState(false)
-
-    useEffect(() => {
-        if (pointcp_id) {
-            fetch(API_URL + `/pointcps/${pointcp_id}`)
-                .then((res) => res.json())
-                .then((data) => {
-                    setPointcpArticle(data.pointcp.data);
-                    setWinner(data.winner)
-                });
-        }
-    }, []);
-
-    const handleVotingClickButtonOne = (e) => {
-        e.preventDefault();
-
-        if (!localStorage.token) {
-            alert("Please sign in to vote");
-        }
-
-        if (localStorage.token !== undefined) {
-            let pointcpData = {
-                pointcp_id: pointcp_id,
-                user_id: user_id,
-                articleOneVote: 1,
-                articleTwoVote: 0,
-            };
-
-            dispatch(voteArticleOne(pointcpData));
-        }
-    };
-
-    const handleVotingClickButtonTwo = (e) => {
-        e.preventDefault();
-
-        if (!localStorage.token) {
-            alert("Please sign in to vote");
-        }
-
-        if (localStorage.token !== undefined) {
-            let pointcpData2 = {
-                pointcp_id: pointcp_id,
-                user_id: user_id,
-                articleOneVote: 0,
-                articleTwoVote: 1,
-            };
-
-            dispatch(voteArticleTwo(pointcpData2));
-        }
-    };
-
-
-    const handleOnClick = () => {
-        setInEditMode(true)
+      dispatch(voteArticleTwo(pointcpData2));
     }
+  };
 
-    const handleOnBack = () => {
-        setInEditMode(false)
-    }
+  const handleOnClick = () => {
+    setInEditMode(true);
+  };
 
-    const dispatch = useDispatch()
+  const handleOnBack = () => {
+    setInEditMode(false);
+  };
 
-    const handleOnDelete = (e) => {
-        e.preventDefault()
-        dispatch(deletePointcp(pointcp_id))
-        history.back('/')
-    }
-    
-    if (pointcpArticle.attributes === undefined) {
-        return (
-            <>
-                <div className="category-show">
-                    <ArticleHeaders category={'point-counterpoint'} />
-                </div>
-                <div>
-                    <h1>LOADING...</h1>
-                </div>
-            </>
-        );
-    } else {
-        return (
-            <>
-                <div className="category-show">
-                    <ArticleHeaders category={'point-counterpoint'} />
-                </div>
-                { inEditMode === false &&
-                    <div className="individualPostDiv" >
-                        <PointcpArticleBody
-                            admin={admin}
-                            article={pointcpArticle.attributes} 
-                            pointcp_id={pointcp_id}
-                            handleVotingClickButtonOne={handleVotingClickButtonOne}
-                            handleVotingClickButtonTwo={handleVotingClickButtonTwo}
-                            handleEditDeleteClick={handleOnClick}
-                            winner={winner}
-                        />
-                    </div>
-                }
-                { inEditMode === true && admin === true && 
-                    <div className="individualPostDiv" >
-                        <PointcpEditForm
-                            setInEditMode={setInEditMode}
-                            pointcpArticle={pointcpArticle.attributes}
-                            pointcp_id={pointcp_id} />
-                        <button onClick={handleOnBack} className="adminButtons">BACK TO ARTICLE</button>
-                        <button onClick={handleOnDelete} className="adminButtons">DELETE ARTICLE</button>
-                    </div>
-                }
-            </>
-        );
-    }
+  const dispatch = useDispatch();
+
+  const handleOnDelete = (e) => {
+    e.preventDefault();
+    dispatch(deletePointcp(pointcp_id));
+    history.back("/");
+  };
+
+  if (pointcpArticle.attributes === undefined) {
+    return (
+      <>
+        <div className="category-show">
+          <ArticleHeaders category={"point-counterpoint"} />
+        </div>
+        <div>
+          <h1>LOADING...</h1>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="category-show">
+          <ArticleHeaders category={"point-counterpoint"} />
+        </div>
+        {inEditMode === false && (
+          <div className="individualPostDiv">
+            <PointcpArticleBody
+              admin={admin}
+              article={pointcpArticle.attributes}
+              pointcp_id={pointcp_id}
+              handleVotingClickButtonOne={handleVotingClickButtonOne}
+              handleVotingClickButtonTwo={handleVotingClickButtonTwo}
+              handleEditDeleteClick={handleOnClick}
+              winner={winner}
+            />
+            <div className="linkToArchives-Container">
+              <Link
+                Link
+                to="/Point-Counterpoint-Archives"
+                className="linkToArchives"
+              >
+                POINT-COUNTERPOINT ARCHIVES
+              </Link>
+            </div>
+          </div>
+        )}
+        {inEditMode === true && admin === true && (
+          <div className="individualPostDiv">
+            <PointcpEditForm
+              setInEditMode={setInEditMode}
+              pointcpArticle={pointcpArticle.attributes}
+              pointcp_id={pointcp_id}
+            />
+            <button onClick={handleOnBack} className="adminButtons">
+              BACK TO ARTICLE
+            </button>
+            <button onClick={handleOnDelete} className="adminButtons">
+              DELETE ARTICLE
+            </button>
+          </div>
+        )}
+      </>
+    );
+  }
 }
 
 export default withRouter(PointCounterPointArticle);
