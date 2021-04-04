fa0de2479f8f7900d0cfd1dc1acdb965b2772611
mattenbar <matthw.enbar@gmail.com>
2021-04-04 13:22:45 -0400

    links bar css works safari and chrome

--- src/components/home/linksbars/linksBarWeekTwo.js
+++ src/components/home/linksbars/linksBarWeekTwo.js
@@ -1,26 +1,34 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
+
 class LinksBarWeekTwo extends Component {
   render(){
     return(
       <div className="linksBarWeekTwo-container">
-        <div className="linksBarWeekTwo-links">
-          <div className="linksBarWeekTwo-Top">
-            <Link to="/Letters-From-Larry">LETTERS FROM LARRY</Link>&nbsp; | &nbsp;
-            <Link to="/Bizarre-Industry-Bazaar">BIZARRE INDUSTRY BAZAAR</Link>&nbsp; | &nbsp;
-            <Link to="/Capital-Connections">CAPITAL CONNECTIONS</Link>&nbsp; | &nbsp;
-            <Link to="/The-Digital-Domain">THE DIGITAL DOMAIN</Link>&nbsp; | &nbsp;
-            <Link to="/Investment-Solutions-and-Gatekeepers">INVESTMENT SOLUTIONS & GATEKEEPERS</Link>
-          </div>
-          <div className="linksBarWeekTwo-Bottom">
-            <Link to="/NewsMakers-Roundup">NEWSMAKERS ROUNDUP</Link>&nbsp; | &nbsp;
-            <Link to="/On-The-Rise">ON THE RISE</Link>&nbsp; | &nbsp;
-            <Link to="/Point-Counterpoint">POINT-COUNTERPOINT</Link> &nbsp; | &nbsp;
-            <Link to="/Statshots">STATSHOTS</Link>&nbsp; | &nbsp;
-            <Link to="/Words-In-Edgewise">WORDS IN EDGEWISE</Link>&nbsp; | &nbsp;
-            <Link to="/Wallet-Share">WALLET SHARE</Link>
-          </div>
+        <div className="linksBarWeekTwo-links"> 
+          
+            <div className="linksBarWeekTwo-Top">
+              
+                <Link to="/Letters-From-Larry">LETTERS FROM LARRY</Link>&nbsp; | &nbsp;
+                <Link to="/Bizarre-Industry-Bazaar">BIZARRE INDUSTRY BAZAAR</Link>&nbsp; | &nbsp;
+                <Link to="/Capital-Connections">CAPITAL CONNECTIONS</Link>&nbsp; | &nbsp;
+                <Link to="/The-Digital-Domain">THE DIGITAL DOMAIN</Link>&nbsp; | &nbsp;
+                <Link to="/Investment-Solutions-and-Gatekeepers">INVESTMENT SOLUTIONS & GATEKEEPERS</Link>
+              
+            </div>
+          
+          
+            <div className="linksBarWeekTwo-Bottom">
+              
+                <Link to="/NewsMakers-Roundup">NEWSMAKERS ROUNDUP</Link>&nbsp; | &nbsp;
+                <Link to="/On-The-Rise">ON THE RISE</Link>&nbsp; | &nbsp;
+                <Link to="/Point-Counterpoint">POINT-COUNTERPOINT</Link> &nbsp; | &nbsp;
+                <Link to="/Statshots">STATSHOTS</Link>&nbsp; | &nbsp;
+                <Link to="/Words-In-Edgewise">WORDS IN EDGEWISE</Link>&nbsp; | &nbsp;
+                <Link to="/Wallet-Share">WALLET SHARE</Link>
+              
+            </div>
           
         </div>
 
