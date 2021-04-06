3d0b5671bcfe1f159bd06c71e1f37850999a5465
mattenbar <matthw.enbar@gmail.com>
2021-04-06 11:15:17 -0400

    changed api url

--- src/apiConstants.js
+++ src/apiConstants.js
@@ -1,4 +1,5 @@
 // local host url:
-export const API_URL = "http://localhost:3000/"
+export const API_URL = "https://wealth-solutions-report.herokuapp.com/"
+// export const API_URL = "http://localhost:3000/"
 
 // hosted website url:
